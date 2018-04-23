7d764ca944586206c68f27354fbff7aa6f1aa895
kw123 <karlwachs@me.com>
2018-04-23 11:34:00 -0500

    first release on github
    first release on github

---
+++ fingscan.indigoPlugin/Contents/Server Plugin/fingEXE/bin/fing-uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Copyright 2012 by Overlook
+
+rm -f /usr/bin/fing
+rm -f /usr/bin/fing-uninstall.sh
+rm -rf /usr/lib/fing
+rm -rf /etc/fing
+rm -rf /usr/share/fing
+rm -f /Library/LaunchDaemons/com.overlooksoft.FingSentinel.plist
+rm -rf /var/log/fing
+rm -rf /var/data/fing
